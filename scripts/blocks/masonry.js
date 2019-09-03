7388cc6b9d31ead756d647fe8de4211348a4c7d3
Симусев Леонид <simusev@techart.ru>
2019-09-03 16:56:48 +0300

    refresh

--- scripts/blocks/masonry.js
+++ scripts/blocks/masonry.js
@@ -1,7 +1,32 @@
+import Colcade from "colcade";
+
 const block = 'b-masonry';
 
 export class Masonry {
-	constructor() {
+	constructor(element) {
+		if (element instanceof Element) {
+			this.init();
+		} else {
+			return null;
+		}
+
+		let result = null;
+
+		if (typeof element === 'string') {
+			result = [...document.querySelectorAll(element)];
+		}
+
+		if (Array.isArray(element)) {
+
+		}
+		const grid = document.querySelector('.grid');
+		const colc = new Colcade( grid, {
+			columns: '.grid-col',
+			items: '.grid-item'
+		});
+	}
+
+	init() {
 
 	}
 }
